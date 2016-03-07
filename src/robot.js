1aad91e0a0d184f5758e7d5e515baa58a91b3626
Shengzhou Zhang <steven.shengzhou@gmail.com>
2016-03-07 16:26:37 +1100

    change initial state from null to {

--- src/robot.js
+++ src/robot.js
@@ -6,7 +6,6 @@ import FACE_TYPES from './faceTypes';
 import CONFIG from './config';
 import deepFreeze from 'deep-freeze-node';
 
-
 const TABLE_SIZE = CONFIG.TABLE_SIZE;
 
 export default function executeInput (filePath) {
@@ -16,7 +15,7 @@ export default function executeInput (filePath) {
 }
 
 export function execute (actions) {
-  const initialState = null;
+  const initialState = {};
   return _.reduce(actions, (state, action) => reducer(state, action), initialState);
 }
 
@@ -40,7 +39,7 @@ export function reducer (previousState, action = {}) {
     case ACTION_TYPES.REPORT:
       return report(previousState);
     default:
-      console.error(`invalid action type: ${action.type}`);
+      // console.error(`invalid action type: ${action.type}`);
       return { ...previousState };
   }
 }
@@ -67,7 +66,7 @@ export function move (previousState) {
     case FACE_TYPES.WEST:
       return { x: _.max([ previousX - 1, 0 ]), y: previousY, face: previousFace };
     default:
-      console.error(`invalid face: ${previousFace}`);
+      // console.error(`invalid face: ${previousFace}`);
       return { ...previousState };
   }
 }
@@ -87,7 +86,7 @@ export function left (previousState) {
     case FACE_TYPES.WEST:
       return { x: previousX, y: previousY, face: FACE_TYPES.SOUTH };
     default:
-      console.error(`invalid face: ${previousFace}`);
+      // console.error(`invalid face: ${previousFace}`);
       return { ...previousState };
   }
 }
@@ -107,12 +106,16 @@ export function right (previousState) {
     case FACE_TYPES.WEST:
       return { x: previousX, y: previousY, face: FACE_TYPES.NORTH };
     default:
-      console.error(`invalid face: ${previousFace}`);
+      // console.error(`invalid face: ${previousFace}`);
       return { ...previousState };
   }
 }
 
 export function report (previousState) {
-  console.log(formatOutput(previousState));
+  if (previousState.x !== undefined &&
+      previousState.y !== undefined &&
+      previousState.face !== undefined) {
+    console.log(formatOutput(previousState));
+  }
   return { ...previousState };
 }
